7dfb1f752ac9892d4c48759b0946481b192c8014
Andor <david-andor@kozpontiagy.hu>
2025-02-16 19:09:12 +0100

    Replace Codeception with PhpUnit

---
+++ src/Phpstan.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Pamald\Robo\PamaldNpm;
+
+/**
+ * @phpstan-type RoboPamaldNpmCollectPackagesTaskOptions = robo-pamald-npm-collect-packages-task-options
+ * @phpstan-type RoboPamaldNpmModifyCommitMsgPartsTaskOptions = robo-pamald-npm-modify-commit-msg-parts-task-options
+ */
+class Phpstan
+{
+}
